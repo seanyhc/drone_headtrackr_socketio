40fc6028c08ad2ab4ed7ec79515242462af91693
Sean Chen <sean.yhc@gmail.com>
2013-04-29 18:03:32 -0700

    1st commit: headtracking and push to server to control ar-drone
    *need to npm install ar-drone & socket.io
    takeoff and langing is manually controlled

---
+++ server.js
@@ -0,0 +1,25 @@
+var io = require('socket.io').listen(8000);
+arDrone = require('ar-drone');
+var drone = arDrone.createClient();
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+    if(data == 'takeoff') {
+    	drone.takeoff();
+    } else if(data =='land') {
+    	drone.land();
+    } else if(data =='right') {
+    	drone.counterClockwise(1);
+    } else if (data =='left') {
+    	drone.clockwise(1);
+    } else if (data == 'hover') {
+    	drone.stop();
+    }
+
+  });
+});
+
+
+
